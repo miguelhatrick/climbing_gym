a869b0007f29502ff6bd5f44567dddb872cecf98
Miguel <miguelhatrick@gmail.com>
2020-10-02 20:54:55 -0300

    Update member_access_package.py

--- models/member_access_package.py
+++ models/member_access_package.py
@@ -48,7 +48,7 @@ class MemberAccessPackage(models.Model):
             _map.date_start = False
             _map.date_finish = False
             _map.completed_date = False
-            _map.obs += "\r\n Revived on %s by %s" % (datetime.now(), str(self.env.user.name))
+            _map.obs = _map.obs + "\r\n Revived on %s by %s" % (datetime.now(), str(self.env.user.name))
             _map.state = 'pending'
 
     @api.multi
