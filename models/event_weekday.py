88740bca20efc0ac1316dbe8590f416a6e551391
Miguel <miguelhatrick@gmail.com>
2020-12-28 02:50:57 -0300

    minor fixes

--- models/event_weekday.py
+++ models/event_weekday.py
@@ -6,6 +6,7 @@ from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from odoo.tools import image_resize_images, image_resize_image, base64
+import calendar
 
 
 class EventWeekday(models.Model):
@@ -15,3 +16,9 @@ class EventWeekday(models.Model):
 
     day_id = fields.Integer('Id for date operations')
     name = fields.Char('WeekdayName')
+
+    localized_name = fields.Char(string='Localized weekday', compute='_get_localized_name')
+
+    def _get_localized_name(self):
+        for _wd in self:
+            _wd.localized_name = calendar.day_name[_wd.day_id]
