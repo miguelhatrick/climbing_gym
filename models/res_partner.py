af8d36b0873355328696bbfcbf1ce5ee55022382
Miguel <miguelhatrick@gmail.com>
2021-10-28 19:05:32 -0300

    fix permission problem when answering tickets

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,10 +1,13 @@
 # Copyright (C)
 # Copyright 2020- (<http://www.a>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import logging
 import pdb
 from datetime import datetime
 
 from odoo import fields, models, api
+
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
@@ -39,58 +42,66 @@ class ResPartner(models.Model):
         string='Current partner has a VALID membership (overdue, active, pending payment)',
         compute='_get_current_partner_membership_valid')
 
+    climbing_gym_medical_certificate_due_date = fields.Date(
+        'Medical Certificate due date',
+        compute='update_certificate_due_date',
+        store=True,
+        compute_sudo=True)
 
-    climbing_gym_medical_certificate_due_date = fields.Date('Medical Certificate due date',
-                                                            compute='update_certificate_due_date', store=True)
-    climbing_gym_medical_certificate_valid = fields.Boolean('Medical certificate valid',
-                                                            compute='update_certificate_status')
+    climbing_gym_medical_certificate_valid = fields.Boolean(
+        'Medical certificate valid',
+        compute='update_certificate_status',
+        compute_sudo=True)
 
-    climbing_gym_medical_certificate_latest = fields.Many2one('climbing_gym.medical_certificate',
-                                                              'Latest confirmed certificate',
-                                                              compute='_get_latest_certificate')
+    climbing_gym_medical_certificate_latest = fields.Many2one(
+        'climbing_gym.medical_certificate',
+        'Latest confirmed certificate',
+        compute='_get_latest_certificate',
+        compute_sudo=True)
 
     @api.one
     @api.depends('climbing_gym_medical_certificate_due_date')
     def update_certificate_status(self):
-        _todaydate = datetime.now().date()
-        self.climbing_gym_medical_certificate_valid = False
+        _today_date = datetime.now().date()
 
-        if self.climbing_gym_medical_certificate_due_date is not False:
-            self.climbing_gym_medical_certificate_valid = self.climbing_gym_medical_certificate_due_date >= _todaydate
+        for _partner in self:
+            _partner.climbing_gym_medical_certificate_valid = False
+
+            if _partner.climbing_gym_medical_certificate_due_date is not False:
+                _partner.climbing_gym_medical_certificate_valid = _partner.climbing_gym_medical_certificate_due_date >= _today_date
 
     @api.one
     @api.depends('climbing_gym_medical_certificates')
     def update_certificate_due_date(self):
-        self.climbing_gym_medical_certificate_valid = False
-        self.climbing_gym_medical_certificate_due_date = None
+        for _partner in self:
 
-        for _certificate in self.climbing_gym_medical_certificates:
-            if _certificate.state != 'confirmed':
-                continue
-            # self.climbing_gym_medical_certificate_due_date = certi.due_date
-            if self.climbing_gym_medical_certificate_due_date is False or self.climbing_gym_medical_certificate_due_date < _certificate.due_date:
-                self.climbing_gym_medical_certificate_due_date = _certificate.due_date
+            _partner.climbing_gym_medical_certificate_valid = False
+            _partner.climbing_gym_medical_certificate_due_date = None
+
+            for _certificate in _partner.sudo().climbing_gym_medical_certificates:
+                if _certificate.state != 'confirmed':
+                    continue
+
+                if _partner.climbing_gym_medical_certificate_due_date is False or _partner.climbing_gym_medical_certificate_due_date < _certificate.due_date:
+                    _partner.climbing_gym_medical_certificate_due_date = _certificate.due_date
 
     def _get_current_partner_membership_active(self):
         for _partner in self:
             _partner.climbing_gym_member_membership_active = False
-            for _membership in _partner.climbing_gym_member_membership_ids:
+
+            for _membership in _partner.sudo().climbing_gym_member_membership_ids:
                 if _membership.state == 'active':
                     _partner.climbing_gym_member_membership_active = True
                     break
 
-
     def _get_current_partner_membership_valid(self):
         for _partner in self:
             _partner.climbing_gym_member_membership_valid = False
-            for _membership in _partner.climbing_gym_member_membership_ids:
+
+            for _membership in _partner.sudo().climbing_gym_member_membership_ids:
                 if _membership.get_state_valid():
                     _partner.climbing_gym_member_membership_valid = True
                     break
-
-
-
-
 
     def _get_latest_certificate(self):
         for _partner in self:
@@ -108,8 +119,9 @@ class ResPartner(models.Model):
         Updates the main membership based on status
         :return: None
         """
+        _partners = self.sudo()
 
-        for _partner in self:
+        for _partner in _partners:
             for _membership in _partner.climbing_gym_member_membership_ids:
                 if _membership.get_state_valid():
                     _partner.climbing_gym_main_member_membership_id = _membership
